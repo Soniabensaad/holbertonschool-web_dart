4766b255b6ee292d898a6d23cba0b7181310c947
sonia <https://github.com/Soniabensaad>
2024-03-28 21:21:10 +0100

    dart

--- 0x02-dart-classes/3-private_password.dart
+++ 0x02-dart-classes/3-private_password.dart
@@ -3,19 +3,38 @@ class Password {
 
   Password({required String password}) : _password = password;
 
-  String get password => _password;
+  bool isValid() {
+    if (_password.length < 8 || _password.length > 16) {
+      return false;
+    }
 
-  set password(String newPassword) {
-    _password = newPassword;
+    if (!_containsUppercase() || !_containsLowercase() || !_containsNumbers()) {
+      return false;
+    }
+
+    return true;
   }
 
-  bool isValid() {
-    return (_password.length >= 8 && _password.length <= 16) &&
-        _password.contains(RegExp(r"[A-Z]")) &&
-        _password.contains(RegExp(r"[a-z]")) &&
-        _password.contains(RegExp(r"[0-9]"));
+  bool _containsUppercase() {
+    return _password.contains(RegExp(r'[A-Z]'));
+  }
+
+  bool _containsLowercase() {
+    return _password.contains(RegExp(r'[a-z]'));
+  }
+
+  bool _containsNumbers() {
+    return _password.contains(RegExp(r'[0-9]'));
+  }
+
+  String get password => _password;
+
+  set password(String value) {
+    _password = value;
   }
 
   @override
-  String toString() => "Password $_password";
+  String toString() {
+    return "Your Password is: $_password";
+  }
 }
