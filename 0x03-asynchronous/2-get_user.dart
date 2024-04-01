6ae629a50ca7c2ea9cd1551a5f5fd4a7815726a3
sonia <https://github.com/Soniabensaad>
2024-04-01 23:30:27 +0200

    dart

---
+++ 0x03-asynchronous/2-get_user.dart
@@ -0,0 +1,11 @@
+import 'dart:convert';
+import '2-util.dart';
+
+Future<void> getUser() async {
+  try {
+    String userData = await fetchUser();
+    print(userData);
+  } catch (error) {
+    print("error caught: $error");
+  }
+}
