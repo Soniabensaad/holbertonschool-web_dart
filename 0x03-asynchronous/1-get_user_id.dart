52c03189a8556ad3503f52e4907c9800b2c79814
sonia <https://github.com/Soniabensaad>
2024-04-01 23:19:18 +0200

    dart

--- 0x03-asynchronous/1-get_user_id.dart
+++ 0x03-asynchronous/1-get_user_id.dart
@@ -1,6 +1,6 @@
+import 'dart:convert';
 import '1-util.dart';
 
-Future<String> getUserId() async {
-  return await fetchUserData();
-
+Future<String> getUserId() {
+  return fetchUserData().then((user) => json.decode(user)['id']);
 }
