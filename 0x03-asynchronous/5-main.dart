7fe6889d3cf5f6fea019f9fd554b61cdaeb13df1
sonia <https://github.com/Soniabensaad>
2024-04-03 18:35:39 +0200

    dart

---
+++ 0x03-asynchronous/5-main.dart
@@ -0,0 +1,13 @@
+import 'rick_and_morty.dart';
+
+main() async {
+  await printRmCharacters();
+}
+achref@achref:~$
+achref@achref:~$ dart 5-main.dart
+Rick Sanchez
+Morty Smith
+Summer Smith
+…
+Amish Cyborg
+Annie
